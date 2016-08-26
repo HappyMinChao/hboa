b6fa1958a522fc60aee66f021b16f19e09af42c0
HappyMinChao <du_minchao@163.com>
2016-08-26 10:38:29 +0800

    请假系统工作流系统

---
+++ a_employee_insert.sql
@@ -0,0 +1,10 @@
+﻿
+INSERT  INTO `a_employee`(`name`,`password`,`email`,`role`,`manager_id`) VALUES 
+('王中军','123','ee@163.com','boss',NULL),
+('冯小刚经纪人','123','dd@163.com','manager',1),
+('范冰冰经纪人','123','cc@163.com','manager',1),
+('冯小刚','123','bb@163.com','user',2),
+('范冰冰','123','aa@163.com','user',3)
+
+
+
---
+++ src/cn/itcast/ssh/service/IEmployeeService.java
@@ -0,0 +1,11 @@
+﻿package cn.itcast.ssh.service;
+
+import cn.itcast.ssh.domain.Employee;
+
+
+public interface IEmployeeService {
+
+	Employee findEmployeeByName(String name);
+
+
+}
